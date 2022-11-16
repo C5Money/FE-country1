bfea7244a5a4e0bd3110047cc90bad4d76a0fd39
C5Money <misriffi@gmail.com>
2022-11-16 22:23:37 +0100

    Landen

--- src/app.js
+++ src/app.js
@@ -6,18 +6,44 @@ import axios from "axios";
 
 // Schrijf een asynchrone functie die, met behulp van Axios, een GET-request maakt naar het juiste endpoint.
 // Log de response in de console en bestudeer de data goed: hoe is het opgebouwd?
+const URI = "https://restcountries.com/";
+const ENDPOINT = "v2/all";
+
+const unorderedList = document.getElementById("landList");
+
 async function fetchAllCountryInfo(){
     try {
-        const result = await axios.get("https://restcountries.com/v2/all");
+        const result = await axios.get(URI + ENDPOINT);
         console.log(result.data);
+
 
 // Probeer eens om de naam van het allereerste land te loggen in de console, welk pad moet je hiervoor volgen?
         result.data.map((info) => {
-            console.log(info.name);
+
+
+// referentie ul-tag
+            const listItem = document.createElement("li");
+            listItem.setAttribute("class", "land");
+            listItem.textContent = info.name;
+            unorderedList.appendChild(listItem);
         });
-    } catch (e){
-        console.error(e);
+
+
+    } catch (err){
+        const errorMessage = document.getElementById("error-message");
+
+
+// Welke error message is van toepassing
+        if (err.result.status === 404){
+            errorMessage.textContent = "Page Not Found | 404";
+        }
+        if (err.result.status === 500){
+            errorMessage.textContent = "Internal SErver Error | 500";
+        }
     }
 }
 
 fetchAllCountryInfo();
+
+
+
